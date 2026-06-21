1e5f60bb0dda34a37c2336d1a33dc36cda648117
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-21 09:56:21 -0400

    Crea PlanetListViewModel

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/presentation/list/PlanetListViewModel.kt
@@ -0,0 +1,52 @@
+package ucne.edu.consumirapiplanets.presentation.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ucne.edu.consumirapiplanets.data.remote.Resource
+import ucne.edu.consumirapiplanets.domain.usecase.GetPlanetsUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class PlanetListViewModel @Inject constructor(
+    private val getPlanetsUseCase: GetPlanetsUseCase
+): ViewModel() {
+
+    private val _state = MutableStateFlow(PlanetListUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        loadPlanets()
+    }
+
+    fun onEvent(event: PlanetListEvent) {
+        when (event) {
+            is PlanetListEvent.UpdateName -> _state.update {
+                    it.copy(filterName = event.name)
+            }
+            PlanetListEvent.Search -> loadPlanets()
+        }
+    }
+
+    private fun loadPlanets() {
+        viewModelScope.launch {
+            val current = _state.value
+
+            getPlanetsUseCase(name = current.filterName).collect { result ->
+                when (result) {
+                    is Resource.Loading -> _state.update { it.copy(isLoading = true) }
+                    is Resource.Success -> _state.update {
+                        it.copy(isLoading = false, planets = result.data ?: emptyList())
+                    }
+                    is Resource.Error -> _state.update {
+                        it.copy(isLoading = false, error = result.message)
+                    }
+                }
+            }
+        }
+    }
+}
