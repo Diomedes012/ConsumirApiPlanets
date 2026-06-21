5750a6fd4e757258a3bd4d14e346d96d4d4550e5
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-21 09:56:30 -0400

    Crea PlanetListScreen

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/presentation/list/PlanetListScreen.kt
@@ -0,0 +1,128 @@
+package ucne.edu.consumirapiplanets.presentation.list
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import ucne.edu.consumirapiplanets.domain.model.Planet
+import androidx.compose.runtime.getValue
+
+@Composable
+fun PlanetListScreen(
+    viewModel: PlanetListViewModel = hiltViewModel(),
+    onPlanetClick: (Int) -> Unit
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    PlanetListBodyScreen(state = state, onEvent = viewModel::onEvent, onPlanetClick = onPlanetClick)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlanetListBodyScreen(
+    state: PlanetListUiState,
+    onEvent: (PlanetListEvent) -> Unit,
+    onPlanetClick: (Int) -> Unit,
+) {
+    Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = { Text("Planetas Dragon Ball") }) }
+    ) { padding ->
+        Column(
+            modifier = Modifier.padding(padding).fillMaxSize()
+        ) {
+            PlanetFilterSection(
+                name = state.filterName,
+                onEvent = onEvent,
+            )
+
+            if (state.isLoading) {
+                CircularProgressIndicator(  modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+
+            LazyColumn(contentPadding = PaddingValues(16.dp)) {
+                items(state.planets) { planet ->
+                    PlanetItem(planet = planet, onClick = { onPlanetClick(planet.id) })
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PlanetFilterSection(
+    name: String,
+    onEvent: (PlanetListEvent) -> Unit,
+) {
+    ElevatedCard(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { onEvent(PlanetListEvent.UpdateName(it)) },
+                label = { Text("Nombre (ej. Namek)") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Button(
+                onClick = { onEvent(PlanetListEvent.Search) },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text("Buscar")
+            }
+        }
+    }
+}
+
+@Composable
+fun PlanetItem(planet: Planet, onClick: () -> Unit) {
+    ElevatedCard(modifier = Modifier.fillMaxWidth().clickable() { onClick() }) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = planet.image,
+                contentDescription = planet.name,
+                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(planet.name, style = MaterialTheme.typography.titleMedium)
+                Text(
+                    text = if (planet.isDestroyed) "Estado: Destruido" else "Estado: Intacto",
+                    color = if (planet.isDestroyed) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    }
+}
