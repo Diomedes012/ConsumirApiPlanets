d568fa1639906befb87904e2363c2b72f5a42e66
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-21 09:54:38 -0400

    Crea GetPlanetUseCase

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/domain/usecase/GetPlanetsUseCase.kt
@@ -0,0 +1,19 @@
+package ucne.edu.consumirapiplanets.domain.usecase
+
+import kotlinx.coroutines.flow.Flow
+import ucne.edu.consumirapiplanets.data.remote.Resource
+import ucne.edu.consumirapiplanets.domain.Repository.PlanetRepository
+import ucne.edu.consumirapiplanets.domain.model.Planet
+import javax.inject.Inject
+
+class GetPlanetsUseCase @Inject constructor(
+    private val repository: PlanetRepository
+) {
+    operator fun invoke(name: String? = null): Flow<Resource<List<Planet>>> {
+        return if (name.isNullOrBlank()) {
+            repository.getPlanets()
+        } else {
+            repository.getPlanetsByName(name)
+        }
+    }
+}
