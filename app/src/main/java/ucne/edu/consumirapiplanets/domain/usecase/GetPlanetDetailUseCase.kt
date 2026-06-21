dd51e1ba16d9eb98088095a5c17a4d2155e73867
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-21 09:54:28 -0400

    Crea GetPlanetDetailUseCase

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/domain/usecase/GetPlanetDetailUseCase.kt
@@ -0,0 +1,10 @@
+package ucne.edu.consumirapiplanets.domain.usecase
+
+import ucne.edu.consumirapiplanets.domain.Repository.PlanetRepository
+import javax.inject.Inject
+
+class GetPlanetDetailUseCase @Inject constructor(
+    private val repository: PlanetRepository
+) {
+    operator fun invoke(id: Int) = repository.getPlanetDetail(id)
+}
