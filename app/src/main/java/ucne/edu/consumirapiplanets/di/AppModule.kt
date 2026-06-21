c1e426bd3d437c2bc9cb3ab2a74673d9d5fe1df6
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-21 09:49:09 -0400

    Crea AppModule

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/di/AppModule.kt
@@ -0,0 +1,44 @@
+package ucne.edu.consumirapiplanets.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import ucne.edu.consumirapiplanets.data.remote.DragonBallApi
+import ucne.edu.consumirapiplanets.data.remote.remotedatasource.PlanetRemoteDataSource
+import ucne.edu.consumirapiplanets.data.remote.repository.PlanetRepositoryImpl
+import ucne.edu.consumirapiplanets.domain.Repository.PlanetRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): DragonBallApi {
+        return Retrofit.Builder()
+            .baseUrl("https://dragonball-api.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DragonBallApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providePlanetRepository(api: DragonBallApi): PlanetRepository {
+        return PlanetRepositoryImpl(PlanetRemoteDataSource(api))
+    }
+}
