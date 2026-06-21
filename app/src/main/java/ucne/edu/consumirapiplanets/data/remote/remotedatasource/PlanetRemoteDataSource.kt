6ee2563df5183232a7d82ad2fc82df5a9823b3d9
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-21 09:52:35 -0400

    Crea PlanetRemoteDataSource

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/data/remote/remotedatasource/PlanetRemoteDataSource.kt
@@ -0,0 +1,56 @@
+package ucne.edu.consumirapiplanets.data.remote.remotedatasource
+
+import coil.network.HttpException
+import ucne.edu.consumirapiplanets.data.remote.DragonBallApi
+import ucne.edu.consumirapiplanets.data.remote.PlanetDto
+import ucne.edu.consumirapiplanets.data.remote.PlanetResponseDto
+import javax.inject.Inject
+
+class PlanetRemoteDataSource @Inject constructor(
+    private val api: DragonBallApi
+) {
+    suspend fun getPlanets(
+        page: Int,
+        limit: Int,
+        name: String?,
+        isDestroyed: Boolean?
+    ): Result<PlanetResponseDto> {
+        return try {
+            val response = api.getPlanets(page, limit, name, isDestroyed)
+            if (!response.isSuccessful) {
+                Result.failure(Exception("Error de red ${response.code()}"))
+            } else {
+                Result.success(response.body()!!)
+            }
+        } catch (e: HttpException) {
+            Result.failure(Exception("Error de servidor", e))
+        } catch (e: Exception) {
+            Result.failure(Exception("Error desconocido", e))
+        }
+    }
+
+    suspend fun getPlanetsByName(name: String): Result<List<PlanetDto>> {
+        return try {
+            val response = api.getPlanetsByName(name)
+            if (response.isSuccessful) Result.success(response.body()!!)
+            else Result.failure(Exception("Error de red ${response.code()}"))
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getPlanetDetail(id: Int): Result<PlanetDto> {
+        return try {
+            val response = api.getPlanetDetail(id)
+            if (!response.isSuccessful) {
+                Result.failure(Exception("Error de red ${response.code()}"))
+            } else {
+                Result.success(response.body()!!)
+            }
+        } catch (e: HttpException) {
+            Result.failure(Exception("Error de servidor", e))
+        } catch (e: Exception) {
+            Result.failure(Exception("Error desconocido", e))
+        }
+    }
+}
