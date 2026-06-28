bc8f0760f0e412130079a419d694a99ac64d1a5d
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-28 09:23:38 -0400

    Crea CharacterListScreen

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/presentation/characters/list/CharacterListScreen.kt
@@ -0,0 +1,105 @@
+package ucne.edu.consumirapiplanets.presentation.characters.list
+
+import ucne.edu.consumirapiplanets.domain.model.Character
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.lazy.items
+import coil.compose.AsyncImage
+
+@Composable
+fun CharacterListScreen(
+    viewModel: CharacterListViewModel = hiltViewModel(),
+    onCharacterClick: (Int) -> Unit
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    CharacterListBodyScreen(state = state, onEvent = viewModel::onEvent, onCharacterClick = onCharacterClick)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CharacterListBodyScreen(
+    state: CharacterListUiState,
+    onEvent: (CharacterListEvent) -> Unit,
+    onCharacterClick: (Int) -> Unit
+) {
+    Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = { Text("Personajes Dragon Ball") }) }
+    ) { padding ->
+        Column(modifier = Modifier.padding(padding).fillMaxSize()) {
+
+            ElevatedCard (modifier = Modifier.padding(16.dp ).fillMaxWidth()) {
+                Column(modifier = Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                    OutlinedTextField(
+                        value = state.filterName,
+                        onValueChange = { onEvent(CharacterListEvent.UpdateName(it)) },
+                        label = { Text("Buscar personaje (ej. Goku)") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Button (onClick = { onEvent(CharacterListEvent.Search) }, modifier = Modifier.align(
+                        Alignment.End)) {
+                        Text("Buscar")
+                    }
+                }
+            }
+
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+
+            LazyColumn(contentPadding = PaddingValues(16.dp)) {
+                items(state.characters) { character ->
+                    CharacterItem(character = character, onClick = { onCharacterClick(character.id) })
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CharacterItem(character: Character, onClick: () -> Unit) {
+    ElevatedCard(modifier = Modifier.fillMaxWidth().clickable { onClick() }) {
+        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            AsyncImage(
+                model = character.image,
+                contentDescription = character.name,
+                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(character.name, style = MaterialTheme.typography.titleMedium)
+                Text("Raza: ${character.race}", style = MaterialTheme.typography.bodyMedium)
+                Text("Ki Max: ${character.maxKi}", style = MaterialTheme.typography.bodySmall, color = MaterialTheme.colorScheme.secondary)
+            }
+        }
+    }
+}
