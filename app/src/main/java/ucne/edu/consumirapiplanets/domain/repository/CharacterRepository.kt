8bf70e6f8966904d8a0d981b41c05defb35acfc3
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-28 09:19:21 -0400

    Crea CharacterRepository

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/domain/repository/CharacterRepository.kt
@@ -0,0 +1,11 @@
+package ucne.edu.consumirapiplanets.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import ucne.edu.consumirapiplanets.data.remote.Resource
+import ucne.edu.consumirapiplanets.domain.model.Character
+
+interface CharacterRepository {
+    fun getCharacters(page: Int = 1, limit: Int = 10): Flow<Resource<List<Character>>>
+    fun getCharactersByName(name: String): Flow<Resource<List<Character>>>
+    fun getCharacterDetail(id: Int): Flow<Resource<Character>>
+}
