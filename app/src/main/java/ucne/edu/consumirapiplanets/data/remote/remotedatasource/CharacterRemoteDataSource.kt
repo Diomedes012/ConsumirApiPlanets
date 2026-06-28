05447d2ca4275069af53d47affa3483195bf0dd5
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-28 09:21:16 -0400

    Crea CharacterRemoteDataSource

---
+++ app/src/main/java/ucne/edu/consumirapiplanets/data/remote/remotedatasource/CharacterRemoteDataSource.kt
@@ -0,0 +1,34 @@
+package ucne.edu.consumirapiplanets.data.remote.remotedatasource
+
+import ucne.edu.consumirapiplanets.data.remote.DragonBallApi
+import ucne.edu.consumirapiplanets.data.remote.dto.character.CharacterDto
+import ucne.edu.consumirapiplanets.data.remote.dto.character.CharacterResponseDto
+import javax.inject.Inject
+
+class CharacterRemoteDataSource @Inject constructor(
+    private val api: DragonBallApi
+) {
+    suspend fun getCharacters(page: Int = 1, limit: Int = 10): Result<CharacterResponseDto> {
+        return try {
+            val response = api.getCharacters(page, limit)
+            if (response.isSuccessful) Result.success(response.body()!!)
+            else Result.failure(Exception("Error de red ${response.code()}"))
+        } catch (e: Exception) { Result.failure(e) }
+    }
+
+    suspend fun getCharactersByName(name: String): Result<List<CharacterDto>> {
+        return try {
+            val response = api.getCharactersByName(name)
+            if (response.isSuccessful) Result.success(response.body()!!)
+            else Result.failure(Exception("Error de red ${response.code()}"))
+        } catch (e: Exception) { Result.failure(e) }
+    }
+
+    suspend fun getCharacterDetail(id: Int): Result<CharacterDto> {
+        return try {
+            val response = api.getCharacterDetail(id)
+            if (response.isSuccessful) Result.success(response.body()!!)
+            else Result.failure(Exception("Error de red ${response.code()}"))
+        } catch (e: Exception) { Result.failure(e) }
+    }
+}
