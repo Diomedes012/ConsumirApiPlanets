7bb94461f031a5cebb3121c8431bb814e3f2748e
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-06-28 09:49:35 -0400

    Agrega las variables filterGender y filterRace a CharacterListUiState.kt

--- app/src/main/java/ucne/edu/consumirapiplanets/presentation/characters/list/CharacterListUiState.kt
+++ app/src/main/java/ucne/edu/consumirapiplanets/presentation/characters/list/CharacterListUiState.kt
@@ -5,5 +5,7 @@ data class CharacterListUiState(
     val isLoading: Boolean = false,
     val characters: List<Character> = emptyList(),
     val error: String? = null,
-    val filterName: String = ""
+    val filterName: String = "",
+    val filterGender: String = "",
+    val filterRace: String = ""
 )
